4a5dd68fb45c60fd4213f8c2de54dc6ddb2a2230
Anthony Dahlberg <anthony-dahlberg@uiowa.edu>
2012-11-14 12:58:48 -0600

    More commenting for will

--- forumapp/models.py
+++ forumapp/models.py
@@ -15,7 +15,7 @@ def initialize_base(engine):
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)
 
-
+# Many (posts) to one (user)
 class Post(Base):
     #table containing all posts
     __tablename__ = 'posts'
@@ -26,7 +26,8 @@ class Post(Base):
     date = Column(DateTime)
     user_id = Column(Integer, ForeignKey('users.id'))
     ## need to add creater id and link to user ##
-
+# One (user) to many (post)
+# One (user) to many (comment)
 class User(Base):
     #table containing all users
     __tablename__ = 'users'
@@ -36,7 +37,8 @@ class User(Base):
     password = Column(String, nullable = False) #encryption or something should be added. check later.
     email = Column(String) # in-case we do email authentication
     age = Column(String)
-
+# Many (comments) to one (post)
+# Many (comments) to one (user)
 class Comment(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
